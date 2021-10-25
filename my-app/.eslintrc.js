bebe066e5d0cc6e3ebfa40ab26be0054650bd764
Oskari Noppa <oskari@arpakuut.io>
2021-10-26 00:32:47 +0300

    Add some basic components

--- my-app/.eslintrc.js
+++ my-app/.eslintrc.js
@@ -19,7 +19,9 @@ module.exports = {
 		sourceType: "module",
 	},
 	plugins: ["react", "jest"],
-	rules: {},
+	rules: {
+		"react/prop-types": "off",
+	},
 	settings: {
 		react: {
 			version: "detect",
---
+++ my-app/src/ProfilePage/types.d.ts
@@ -0,0 +1,9 @@
+
+export interface UserInfo {
+	name: string
+	avatarUrl: string
+}
+
+export interface ProfilePageProps {
+	user: UserInfo
+}
