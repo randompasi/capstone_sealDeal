2fb705aa2653174cfa5922f7eacb5adbe1de165c
Antti Vuorinen <antti.vuorinen@vincit.fi>
2021-11-05 08:16:23 +0200

    adding master branch

--- deployment/terraform/locals.tf
+++ deployment/terraform/locals.tf
@@ -50,4 +50,14 @@ locals {
             destination_address_prefix = "*"
         }
     }
+
+    
+    prefix   = "capstone"
+  backend_address_pool_name      = "${azurerm_virtual_network.capstoneterraformnetwork.name}-beap"
+  frontend_port_name             = "${azurerm_virtual_network.capstoneterraformnetwork.name}-feport"
+  frontend_ip_configuration_name = "${azurerm_virtual_network.capstoneterraformnetwork.name}-feip"
+  http_setting_name              = "${azurerm_virtual_network.capstoneterraformnetwork.name}-be-htst"
+  listener_name                  = "${azurerm_virtual_network.capstoneterraformnetwork.name}-httplstn"
+  request_routing_rule_name      = "${azurerm_virtual_network.capstoneterraformnetwork.name}-rqrt"
+  redirect_configuration_name    = "${azurerm_virtual_network.capstoneterraformnetwork.name}-rdrcfg"
 }
--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -16,7 +16,11 @@ locals {
 
 
 provider "azurerm" {
-  features {}
+  features {
+     key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+  }
   subscription_id   =  local.credentials.subscrition
   tenant_id         = local.credentials.tenant
   client_id         = local.credentials.appId
@@ -45,6 +49,10 @@ resource "azurerm_virtual_network" "capstoneterraformnetwork" {
   }
 }
 
+
+output "account_id" {
+  value = data.azurerm_client_config.current.client_id
+}
 # Create subnet
 resource "azurerm_subnet" "capstoneterraformsubnet" {
   name                 = "capstoneSubnet"
@@ -58,12 +66,114 @@ resource "azurerm_public_ip" "capstonePublicIP" {
   name                         = "capstonePublicIP"
   location                     = var.azure_region
   resource_group_name          = azurerm_resource_group.capstoneterraformgroup.name
-  allocation_method            = "Dynamic"
-  idle_timeout_in_minutes = 30
+  allocation_method            = "Static"
+  idle_timeout_in_minutes      = 30
+  domain_name_label            = "capstoneseal" 
+  tags                         = azurerm_resource_group.capstoneterraformgroup.tags 
+}
 
-  tags = {
-    environment = "Terraform Demo"
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "agw" {
+  name                       = "capstone3"
+  location                   = azurerm_resource_group.capstoneterraformgroup.location
+  resource_group_name        = azurerm_resource_group.capstoneterraformgroup.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  sku_name                   = "standard"
+
+
+  tags = azurerm_resource_group.capstoneterraformgroup.tags
+
+   access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    certificate_permissions = [
+      "create",
+    "get",
+    "list"
+    ]
+
+    key_permissions = [
+         "get", "list"
+    ]
+
+    secret_permissions = [
+      "Get","list","set","delete"
+    ]
+    storage_permissions = [
+      "Get",
+    ]
   }
+}
+
+resource "azurerm_key_vault_secret" "example" {
+  name         = "secret-sauce"
+  value        = "szechuan"
+  key_vault_id = azurerm_key_vault.agw.id
+}
+
+resource "azurerm_key_vault_certificate" "mysite1" {
+  name         = "capstoneSite"
+  key_vault_id = azurerm_key_vault.agw.id
+
+  certificate_policy {
+    issuer_parameters {
+      name = "Self"
+    }
+
+    key_properties {
+      exportable = true
+      key_size   = 2048
+      key_type   = "RSA"
+      reuse_key  = true
+      }
+
+    lifetime_action {
+      action {
+        action_type = "AutoRenew"
+      }
+
+      trigger {
+        days_before_expiry = 30
+      }
+    }
+
+    secret_properties {
+      content_type = "application/x-pkcs12"
+    }
+
+    x509_certificate_properties {
+      # Server Authentication = 1.3.6.1.5.5.7.3.1
+      # Client Authentication = 1.3.6.1.5.5.7.3.2
+      extended_key_usage = ["1.3.6.1.5.5.7.3.1"]
+
+      key_usage = [
+        "cRLSign",
+        "dataEncipherment",
+        "digitalSignature",
+        "keyAgreement",
+        "keyCertSign",
+        "keyEncipherment",
+      ]
+
+      subject_alternative_names {
+        dns_names = ["capstoneseal.northeurope.cloudapp.azure.com"]
+      }
+
+      subject            = "CN=capstoneseal.northeurope.cloudapp.azure.com"
+      validity_in_months = 12
+    }
+  }
+}
+
+resource "time_sleep" "wait_60_seconds" {
+  depends_on = [azurerm_key_vault_certificate.mysite1]
+
+  create_duration = "60s"
 }
 
 
@@ -85,6 +195,110 @@ resource "azurerm_network_interface" "capstoneTerraformnic" {
     environment = "Terraform Demo"
   }
 }
+
+
+
+
+# resource "azurerm_application_gateway" "network" {
+#   depends_on          = [azurerm_key_vault_certificate.mysite1, time_sleep.wait_60_seconds]
+#   name                = "capstone-appgateway"
+#   resource_group_name = azurerm_resource_group.capstoneterraformgroup.name
+#   location            = var.azure_region
+#   enable_http2        = true
+
+#   sku {
+#     name     = "Standard_Small"
+#     tier     = "Standard"
+#     capacity = 2
+#   }
+
+#   gateway_ip_configuration {
+#     name      = "my-gateway-ip-configuration"
+#     subnet_id = azurerm_subnet.capstoneterraformsubnet.id
+#   }
+
+#    ssl_certificate {
+#     name                = azurerm_key_vault_certificate.mysite1.name
+#     key_vault_secret_id = azurerm_key_vault_certificate.mysite1.secret_id
+#   }
+
+
+#   identity {
+#     type         = "UserAssigned"
+#     identity_ids = [azurerm_user_assigned_identity.agw.id]
+#   }
+
+#     frontend_ip_configuration {
+#     name                 = "frontend-public"
+#     public_ip_address_id = azurerm_public_ip.capstonePublicIP.id
+#   }
+
+#    frontend_port {
+#     name = "${local.frontend_port_name}-80"
+#     port = 80
+#   }
+
+#   frontend_port {
+#     name = "${local.frontend_port_name}-443"
+#     port = 443
+#   }
+
+
+#   backend_address_pool {
+#     name  = local.backend_address_pool_name
+#   }
+
+#    backend_http_settings {
+#     name                  = "backendSetting"
+#     cookie_based_affinity = "Disabled"
+#     path                  = ""
+#     port                  = 80
+#     protocol              = "Http"
+#     request_timeout       = 60
+#   }
+
+#    http_listener {
+#     name                           = "${local.listener_name}-http"
+#     frontend_ip_configuration_name = "${local.frontend_ip_configuration_name}-public"
+#     frontend_port_name             = "${local.frontend_port_name}-80"
+#     protocol                       = "Http"
+#   }
+
+#   http_listener {
+#     name                           = "${local.listener_name}-https"
+#     frontend_ip_configuration_name = "${local.frontend_ip_configuration_name}-public"
+#     frontend_port_name             = "${local.frontend_port_name}-443"
+#     protocol                       = "Https"
+#     ssl_certificate_name           = azurerm_key_vault_certificate.mysite1.name
+#   }
+
+#    request_routing_rule {
+#     name                       = "${local.request_routing_rule_name}-https"
+#     rule_type                  = "Basic"
+#     http_listener_name         = "${local.listener_name}-https"
+#     backend_address_pool_name  = local.backend_address_pool_name
+#     backend_http_settings_name = local.http_setting_name
+#   }
+
+#   redirect_configuration {
+#     name                 = local.redirect_configuration_name
+#     redirect_type        = "Permanent"
+#     include_path         = true
+#     include_query_string = true
+#     target_listener_name = "${local.listener_name}-https"
+#   }
+
+#   request_routing_rule {
+#     name                        = "${local.request_routing_rule_name}-http"
+#     rule_type                   = "Basic"
+#     http_listener_name          = "${local.listener_name}-http"
+#     redirect_configuration_name = local.redirect_configuration_name
+#   }
+
+
+ 
+
+# }
 
 # Create Network Security Group 
 resource "azurerm_network_security_group" "securityGroup" {
