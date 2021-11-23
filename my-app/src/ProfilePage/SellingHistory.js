29f03c1d2f61481efa0c602d87afaea658ece6bb
Oskari Noppa <oskari@arpakuut.io>
2021-11-23 18:57:50 +0200

    Fix ESLint errors

--- my-app/src/ProfilePage/SellingHistory.js
+++ my-app/src/ProfilePage/SellingHistory.js
@@ -1,65 +1,53 @@
 import Box from "../Box";
-import {render} from "react-dom";
 
-/**
- * @param {{list: ProfilePage.History}} props
- */
-function History(list) {
+function History() {
 	const historyLists = [
 		{
 			id: 0,
-			content: 'Bicycle',
-			date: '22.10.2021',
-			button: 'User Review'
+			content: "Bicycle",
+			date: "22.10.2021",
+			button: "User Review",
 		},
 		{
 			id: 1,
-			content: 'Bicycle',
-			date: '22.10.2021',
-			button: 'User Review'
+			content: "Bicycle",
+			date: "22.10.2021",
+			button: "User Review",
 		},
 		{
 			id: 2,
-			content: 'Bicycle',
-			date: '22.10.2021',
-			button: 'User Review'
+			content: "Bicycle",
+			date: "22.10.2021",
+			button: "User Review",
 		},
 		{
 			id: 3,
-			content: 'Bicycle',
-			date: '22.10.2021',
-			button: 'User Review'
-		}
-	]
+			content: "Bicycle",
+			date: "22.10.2021",
+			button: "User Review",
+		},
+	];
 
-	const listAll = historyLists.map(historyList =>
+	const listAll = historyLists.map((historyList) => (
 		<li key={historyList.id}>
 			{historyList.content}
 			{historyList.date}
-			<div>
-				{historyList.button}
-			</div>
+			<div>{historyList.button}</div>
 		</li>
-	)
+	));
 
 	return (
 		<div>
-			<ul>
-				{listAll}
-			</ul>
+			<ul>{listAll}</ul>
 		</div>
-	)
+	);
 }
 
-/**
- * @param {ProfilePage.ProfilePageProps} props
- */
-// eslint-disable-next-line no-unused-vars
-export default function SellingHistory(user) {
+export default function SellingHistory() {
 	return (
 		<Box className="my-5" title="Selling history">
 			<div className="grid grid-cols-3 w-full">
-				<History list={history} key={history} />
+				<History />
 			</div>
 		</Box>
 	);
