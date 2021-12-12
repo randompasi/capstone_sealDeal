1088e6d106226f41a6e9aaf90f7a4fbb0184db6e
Lassi Haapala <lassi.p.haapala@gmail.com>
2021-12-12 16:49:55 +0200

    Updated settings menu design - eslint errors

--- my-app/src/Navbar.js
+++ my-app/src/Navbar.js
@@ -4,8 +4,6 @@ import {CgLogOut} from "react-icons/cg"; //react-icons.github.io/react-icons
 import {IoSettingsOutline} from "react-icons/io5";
 import logoImage from "./assets/logo.png";
 
-const makeCssUrl = (url) => `url(${url})`;
-
 export default function Navbar({setSettings}) {
 	const {user, signout} = useAuth();
 	if (!user) {
