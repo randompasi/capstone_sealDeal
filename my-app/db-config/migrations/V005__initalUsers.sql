346328e9c1159bd8709f9d5da7f6c23ff7e77c1e
Antti Vuorinen <antti.vuorinen@vincit.fi>
2022-01-03 22:28:26 +0200

    add dummy users

---
+++ my-app/db-config/migrations/V005__initalUsers.sql
@@ -0,0 +1,6 @@
+insert into api.users values ( default, now(), 'Archibd', 'Haddock', default, 'Belfia', '1941-10-17', default);
+insert into api.users values ( default, now(), 'Aku', 'Ankka', default, 'Ankalinna', '1934-03-12', default);
+insert into api.users values ( default, now(), 'Mikki', 'Hiiri', default, 'Kouvola', '1928-08-20', default);
+insert into api.users values ( default, now(), 'Hessu', 'Hopo', default, 'ankalinna', '1930-12-17', default);
+insert into api.users values ( default, now(), 'Asterix', 'Gallialainen', default, 'Gallia', '1934-03-13', default);
+insert into api.users values ( default, now(), 'Iines', 'Ankka', default, 'Ankalinna', '1980-12-17', default);
