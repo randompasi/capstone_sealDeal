bf1f91dd8026f9e3133aa0bf70b2254171a11887
Honghao-Du <73260620+Honghao-Du@users.noreply.github.com>
2022-01-07 01:45:44 +0200

    Add files via upload

---
+++ my-app/src/OfferingPage/OfferingPage.js
@@ -0,0 +1,24 @@
+import Box from "../Box";
+
+// will add components later
+function Components() {
+
+	return(
+		<div></div>
+	)
+
+}
+
+// ...
+
+
+export default function OfferingPage() {
+	return (
+		<Box className="my-5" title="Offering Page">
+			<div className="w-full">
+
+			</div>
+		</Box>
+	);
+}
+
